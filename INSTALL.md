b99d6678d4b014102e900864c7c5342f03a33488
DeadlyChambers <shanechambers85@gmail.com>
2022-10-17 04:42:07 -0600

    VPC and EKS updates

---
+++ INSTALL.md
@@ -0,0 +1,14 @@
+# Create From Scratch
+
+```shell
+alias tf=terraform
+
+cd vpc/examples
+tf init
+tf apply
+
+cd ../../examples
+tf init
+tf apply
+```
+
--- examples/main.tf
+++ examples/main.tf
@@ -1,37 +1,96 @@
+locals {
+    app = "shaneschambers"
+    env = "dev"
+    service = "eks"
+    region = "us-east-1"
+  vpc_name = "${local.env}-${local.app}-vpc"
+  cluster_name = "${local.env}-${local.app}-${local.service}-cluster"
+  tags ={
+    build_number = var.build_number
+    created_by = var.created_by
+    managed_by = var.managed_by
+    "sc.app" = local.app
+    "sc.env" = local.env
+    "sc.service" = local.service
+  }
+}
+
+provider "aws" {
+  default_tags = local.tags
+  region = "${local.region}"
+}
+
+data "aws_vpc"  vpc {
+    name = local.vpc_name
+}
+
+data "aws_subnet" master {
+    name = "${local.vpc_name}-public-${local.region}a"
+}
+
+data "aws_subnets" public_subnets {
+    filter {
+        key = "Name"
+        values = ["${local.vpc_name}-public-*"]
+    }
+}
+
+
+data "aws_subnets" private_subnets {
+    filter {
+        key = "Name"
+        values = ["${local.vpc_name}-private-*"]
+    }
+}
+
 module "kubernetes" {
   source = "scholzj/kubernetes/aws"
 
-  aws_region           = "eu-central-1"
-  cluster_name         = "aws-kubernetes"
+  aws_region           = local.region
+  cluster_name         = local.cluster_name
   master_instance_type = "t2.medium"
   worker_instance_type = "t2.medium"
   ssh_public_key       = "~/.ssh/id_rsa.pub"
   ssh_access_cidr      = ["0.0.0.0/0"]
   api_access_cidr      = ["0.0.0.0/0"]
-  min_worker_count     = 3
-  max_worker_count     = 6
-  hosted_zone          = "my-domain.com"
+  min_worker_count     = 1
+  max_worker_count     = 2
+  hosted_zone          = "shaneschambers.com"
   hosted_zone_private  = false
-
-  master_subnet_id = "subnet-8a3517f8"
-  worker_subnet_ids = [
-    "subnet-8a3517f8",
-    "subnet-9b7853f7",
-    "subnet-8g9sdfv8",
-  ]
+  
+  master_subnet_id = data.aws_subnet.master.id
+  worker_subnet_ids = tolist(data.aws_subnets.public_subnets.ids)
 
   # Tags
-  tags = {
-    Application = "AWS-Kubernetes"
-  }
+  tags = local.tags
 
   # Tags in a different format for Auto Scaling Group
   tags2 = [
     {
-      key                 = "Application"
-      value               = "AWS-Kubernetes"
+      key                 = "build_number"
+      value               = var.build_number
       propagate_at_launch = true
     },
+    {
+      key                 = "created_by"
+      value               = var.created_by
+      propagate_at_launch = true
+    },
+    {
+      key                 = "managed_by"
+      value               = var.managed_by
+      propagate_at_launch = true
+    },
+    {
+        key = "app"
+        value = loca.app
+        propagate_at_launch = true
+    },
+    {
+        key = "env"
+        value = local.env
+        propagate_at_launch = true
+    }
   ]
 
   addons = [
